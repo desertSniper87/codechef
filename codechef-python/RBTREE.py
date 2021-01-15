d6a070fffd4ee0f3c2528113958a47ab370562ec
desertsniper87 <torshobuet@gmail.com>
2021-01-15 16:34:12 +0600

    qa init

--- codechef-python/RBTREE.py
+++ codechef-python/RBTREE.py
@@ -5,8 +5,6 @@ from math import log, log2, floor
 # 5 Qb 90 100 Qb 1 20 Qb 4 1 Qr 74 52 Qb 21 2
 # 1 Qb 1 20
 def calc_rb(n1, n2):
-
-
     def get_lr(n):
         return 2*n, 2*n + 1
 
@@ -30,51 +28,40 @@ def calc_rb(n1, n2):
     line_position_n2 = get_line_position(n2, h_n2)
     relative_position_n2 = get_tree_relative_position(line_position_n2, h_n2)
 
-    # for i in range(1, 30):
-    #     h_n2 = floor(log2(i))
-    #     line_position = i % (2 ** h_n2)
-    #     print(i, get_tree_line_position(line_position, h_n2))
+    count = {
+        'red': 0,
+        'black': 0
+    }
+
+    if h_n2 % 2 == 0:
+        count['black'] += 1
+    else:
+        count['red'] += 1
 
 
-    print("LINE:39 -- n1 = " + str(n1))
     while n1!=n2:
         left_n1, right_n1 = get_lr(n1)
+        h_n1 = get_height(n1)
+
+        if h_n1 % 2 == 0:
+            count['red'] += 1
+        else:
+            count['black'] += 1
+
 
         if n2 == left_n1 or n2 == right_n1:
-            print(n1, n2)
-            print("Found")
-            return
+            return count
         else:
-            h_n1 = get_height(n1)
             n2_parent_at_h = get_root_at_height(n2, h_n2, h_n1+1)
 
             if h_n2 <= h_n1:
                 n1 = n1 // 2
-            # elif n1 == 1:
-            #     if get_tree_relative_position(n2_parent_at_h, 2) == 'left':
-            #         n1 = n1 * 2
-            #     else:
-            #         n1 = (n1 * 2) + 1
-
-                # if get_tree_relative_position(get_line_position(n1, h_n1), h_n1) == relative_position_n2:
-                #     print("LINE:61 -- n1 = " + str(n1))
-                # else:
-                #     n1 = n1 // 2
             elif n1 == 1 or get_tree_relative_position(get_line_position(n1, h_n1), h_n1) == relative_position_n2:
-                # WE NEED TO FIND IF SAME SIDE
                 n1 = n2_parent_at_h
             else:
                 n1 = n1 // 2
 
-
-        #
-        #
-        #
-        # if (n2 > n1):
-        #     n1 = n1 // 2
-            
-
-    # print("LINE:6 -- n1, n2 = " + str(n1, n2))
+    return count
 
 
 if __name__ == '__main__':
@@ -89,15 +76,22 @@ if __name__ == '__main__':
         q_type = vars[index]
 
         if q_type == 'Qi':
-            inverse = True
+            inverse = not inverse
         else:
             n1 = vars[index+1]
             n2 = vars[index+2]
             index += 2
-            calc_rb(int(n1), int(n2))
-            # if q_type == 'Qb':
-            # elif q_type == 'Qr':
-            #     print("Qr", n1, n2)
+            count = calc_rb(int(n1), int(n2))
+            if q_type == 'Qb':
+                if inverse is True:
+                    print(count['red'], end=' ')
+                else:
+                    print(count['black'], end=' ')
+            elif q_type == 'Qr':
+                if inverse is True:
+                    print(count['black'], end=' ')
+                else:
+                    print(count['red'], end=' ')
 
         index += 1
 
